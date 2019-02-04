8467ac66685ec0b285b902c6f971b3ad52e90b0f
Tyler Watson <leggettwatson.tyler@gmail.com>
2019-02-04 14:11:02 -0800

    map state to props and render list and render functions completed to render blog post list api data

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import postsReducer from '/postsReducer.js';
+import postsReducer from './postsReducer.js';
 
 export default combineReducers({
     posts: postsReducer
